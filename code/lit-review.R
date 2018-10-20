396a8a88c7bac455d973ac98d0854cec0fa25dba
f_leejy <franco9505@gmail.com>
2018-10-19 22:27:25 -0400

    Factorize h_rating
    Factor based on (0,3] and (4,5]

--- code/lit-review.R
+++ code/lit-review.R
@@ -1,4 +1,4 @@
-hoq <- read.csv(file="~/Education/Ryerson (2018)/capstone/final_hoq.csv", header = TRUE, stringsAsFactors=TRUE)
+hoq <- read.csv(file="../dataset/final_hoq.csv", header = TRUE, stringsAsFactors=TRUE)
 
 str(hoq)
 
@@ -10,3 +10,7 @@ hoq$h_pex <- ordered(hoq$h_pex, levels = c("'Not Available'", "'Below the nation
 hoq$h_eoc <- ordered(hoq$h_eoc, levels = c("'Not Available'", "'Below the national average'", "'Same as the national average'", "'Above the national average'"))
 hoq$h_toc <- ordered(hoq$h_toc, levels = c("'Not Available'", "'Below the national average'", "'Same as the national average'", "'Above the national average'"))
 hoq$h_imaging <- ordered(hoq$h_imaging, levels = c("'Not Available'", "'Below the national average'", "'Same as the national average'", "'Above the national average'"))
+
+# Categorize hospital overall rating
+hoq$h_rating <- cut(hoq$h_rating, breaks=c(0, 3, 5), labels=c("Low Hospital Quality", "High Hospital Quality"))
+str(hoq)
